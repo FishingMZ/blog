21cd218b20b6bfe85099224e02e2f42b697d6897
FishingMZ <328024312@qq.com>
2026-02-17 20:09:36 +0800

    Update Pio model path in configuration

--- src/config.ts
+++ src/config.ts
@@ -617,7 +617,7 @@ export const sakuraConfig: SakuraConfig = {
 // Pio 看板娘配置
 export const pioConfig: import("./types/config").PioConfig = {
 	enable: true, // 启用看板娘
-	models: ["/blog/public/pio/models/pio/model.json"], // 默认模型路径
+	models: ["/pio/models/pio/model.json"], // 默认模型路径
 	position: "left", // 模型位置
 	width: 280, // 默认宽度
 	height: 250, // 默认高度
