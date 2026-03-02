a24d72255118781840b4bcdf20b373b23d17d54d
FishingMZ <328024312@qq.com>
2026-03-02 20:23:13 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -562,7 +562,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 	components: {
 		left: ["profile", "announcement", "categories", "tags"],
 		right: ["site-stats", "calendar"],
-		drawer: ["profile", "announcement","categories", "tags","site-stats", "calendar"],
+		drawer: ["profile", "announcement","categories", "tags","site-stats"],
 	},
 
 	// 默认动画配置
