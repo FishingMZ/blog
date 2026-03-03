e86f175f5b5a74e2e53a4a4215c4599c38e42bad
FishingMZ <328024312@qq.com>
2026-03-03 18:25:00 +0800

    Refactor devicesData with new device entries
    Updated device descriptions and added OnePlus and Router categories.

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -16,6 +16,48 @@ export type DeviceCategory = {
 };
 
 export const devicesData: DeviceCategory = {
+	Xiaomi: [
+		{
+			name: "Redmi 5 Plus",
+			image: "/images/device/redmi5plus.jpg",
+			specs: "Rose / 4G + 64GB",
+			description:
+				"骁龙625一代神u，刷机小王子，摄像较差，刷入alphadroid安卓13退休中。",
+			link: "https://www.mi.com/redmi5",
+		},
+  		{
+			name: "Black Shark",
+	  		image: "/images/device/blackshark.jpg",
+			specs: "Black / 8G + 128GB",
+			description:
+				"骁龙845，但刷机包极少，不如隔壁的mix2s，摄像一般，刷入lineage安卓15主力中。",
+	  		link: "https://www.mi.com/blackshark",
+		},
+  		{
+			name: "Xiaomi Pad 4 Plus",
+	  		image: "/images/device/xiaomipad4plus.webp",
+			specs: "Black / 4G + 64GB",
+			description:
+				"骁龙660，刷机包挺多，摄像一般（但是Pad不需要摄像），刷入crdroid安卓13养老中。",
+	  	  	link: "https://www.mi.com/mipad4",
+		},
+  		{
+			name: "Xiaomi CC9 Pro",
+	  		image: "/images/device/xiaomicc9pro.jpg",
+			specs: "Blue / 8G + 256GB",
+			description:
+				"骁龙730G一代烂u，但是摄像神了，小米首发一亿像素放到今天依旧能打，吃灰中。",
+		  	link: "https://www.mi.com/micc9",
+		},
+  		{
+			name: "Xiaomi Smart Band 9 Pro",
+	  		image: "/images/device/xiaomismartband9pro.png",
+			specs: "Black / 256MB",
+			description:
+				"续航长。屏幕四边等宽观感舒服，表盘和快应用很多也比较实用，重量质感这一块都还可以。",
+			link: "https://www.mi.com/prod/xiaomi-shouhuan-9-pro",
+		},
+	],
 	OnePlus: [
 		{
 			name: "OnePlus 13T",
@@ -34,38 +76,6 @@ export const devicesData: DeviceCategory = {
 			description:
 				"Portable WiFi 6 router suitable for business trips and home use.",
 			link: "https://www.gl-inet.cn/products/gl-mt3000/",
-		},
-	],
-	Xiaomi: [
-		{
-			name: "Redmi 5 Plus",
-			specs: "Rose / 4G + 64GB",
-			description:
-				"骁龙625一代神u，刷机小王子，我的成色战损（除屏幕以外），摄像一般，刷入alphadroid安卓13退休中。",
-		},
-  {
-			name: "Black Shark",
-			specs: "Black / 8G + 128GB",
-			description:
-				"骁龙845一代神u，但刷机包极少，不如隔壁的mix2s，我的成色还可以，摄像一般，刷入lineage安卓15主力中。",
-		},
-  {
-			name: "Xiaomi Pad 4 Plus",
-			specs: "Black / 4G + 64GB",
-			description:
-				"骁龙845一代神u，刷机包挺多，我的成色还可以，摄像一般（Pad不需要摄像），刷入crdroid安卓13养老中。",
-		},
-  {
-			name: "Xiaomi CC9 Pro",
-			specs: "Blue / 8G + 256GB",
-			description:
-				"骁龙730G一代烂u，未解bootloader，性能不如黑鲨，但是摄像神了，小米首发一亿像素，放到今天依旧能打，只不过芯片孱弱，拍张照都得处理一段时间，我的成色还可以，吃灰中。",
-		},
-  {
-			name: "Xiaomi Smart Band 9 Pro",
-			specs: "Black / 256MB",
-			description:
-				"当初499原价买的，现在感觉有点亏了，不过确实是好用的。屏幕四边等宽观感很舒服，表盘和快应用很多，计算器电子书工具箱小游戏等等比较实用，重量质感这一块都还可以。",
 		},
 	],
 };
