f5c71b11aa61d7c588fbaa11f8a604fb5a2f863f
FishingMZ <328024312@qq.com>
2026-03-27 18:54:37 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -439,7 +439,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
-		envId: "https://twikoo-murec.vercel.app",
+		envId: "https://fish-twikoo.netlify.app",
 		lang: SITE_LANG,
 	},
 };
